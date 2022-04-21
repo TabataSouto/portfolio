73d5b710ecb896f943c88b832fad5229a9c8b993
Tabata <tabatac.souto@gmail.com>
2022-04-21 12:00:25 -0300

    feat: página de projetos pessoais

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import TrybeProjects from "./pages/projects/TrybeProjects";
+import PersonalProjects from "./pages/projects/PersonalProjects";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={ Home } />
         <Route path='/trybe-projects' component={ TrybeProjects } />
+        <Route path='/personal-projects' component={ PersonalProjects } />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/pages/projects/PersonalProjects.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Footer from '../../components/footer/Footer';
+import Header from '../../components/header/Header';
+import './projects.css';
+
+function PersonalProjects() {
+  return(
+    <section>
+      <Header />
+      <main>
+        <div className="loading"></div>
+      </main>
+      <Footer />
+    </section>
+  )
+}
+
+export default PersonalProjects;
