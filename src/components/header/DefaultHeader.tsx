5c32ad9f487f77ad95db704c7cc97b1f299f325d
Tabata Souto <tabatac.souto@gmail.com>
2024-04-13 19:24:20 -0300

    feat: header dinâmico

---
+++ src/components/header/DefaultHeader.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image"
+
+interface IDefaultHeaderProps {
+  icon: string;
+  title: string;
+}
+
+export const DefaultHeader = ({
+  icon,
+  title
+}: IDefaultHeaderProps) => {
+  return (
+    <header className="pt-5 text-white">
+      <div className="flex mx-14 items-center gap-3">
+        <Image src={icon} width={40} height={40} alt="" />
+        <p className="text-2xl">{title}</p>
+      </div>
+      <hr className="mx-6 mt-2" />
+    </header>
+  )
+}
