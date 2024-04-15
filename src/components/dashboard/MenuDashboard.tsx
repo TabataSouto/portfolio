0998af97420c95882a76a4a1099a39d7c876e120
Tabata Souto <tabatac.souto@gmail.com>
2024-04-15 15:09:16 -0300

    feat: icónes menu

--- src/components/dashboard/MenuDashboard.tsx
+++ src/components/dashboard/MenuDashboard.tsx
@@ -12,29 +12,23 @@ export const MenuDashboard = () => {
           <div className="bg-red-menu w-14 h-14 rounded-full flex justify-center">
             <Image src="/user.svg" width={30} height={30} alt="usuário" />
           </div>
-          <p className="text-base font-normal transition ease-in-out delay-150 text-center group-hover:text-aqua2">
+          <p className="text-lg font-normal transition ease-in-out delay-150 text-center group-hover:text-aqua2">
             Usuário
           </p>
         </li>
         <li className="group flex flex-col items-center gap-2 w-14">
           <div className="bg-white dark:bg-dark-mode-3 w-14 h-14 rounded-full flex justify-center">
-            <Image
-              src="/message.svg"
-              width={30}
-              height={30}
-              alt="contato"
-              className="fill-current dark:invert"
-            />
+            <Image src="/message.svg" width={30} height={30} alt="contato" />
           </div>
-          <p className="text-base font-normal transition ease-in-out delay-150 text-center group-hover:text-aqua2">
+          <p className="text-lg font-normal transition ease-in-out delay-150 text-center group-hover:text-aqua2">
             Contato
           </p>
         </li>
         <li className="group flex flex-col items-center gap-2 w-14">
           <div className="bg-white dark:bg-dark-mode-3  w-14 h-14 rounded-full flex justify-center">
-            <Image src="/project.svg" width={30} height={30} alt="projetos" />
+            <Image src="/project.svg" width={32} height={32} alt="projetos" />
           </div>
-          <p className="text-base font-normal transition ease-in-out delay-150 text-center group-hover:text-aqua2">
+          <p className="text-lg font-normal transition ease-in-out delay-150 text-center group-hover:text-aqua2">
             Projetos
           </p>
         </li>
@@ -47,16 +41,36 @@ export const MenuDashboard = () => {
               alt="hard skills"
             />
           </div>
-          <p className="text-base font-normal transition ease-in-out delay-150 text-center group-hover:text-aqua2 w-24">
+          <p className="text-lg font-normal transition ease-in-out delay-150 text-center group-hover:text-aqua2 w-24">
             Hard Skills
           </p>
         </li>
         <li className="group flex flex-col items-center gap-2 w-14">
           <div className="bg-white dark:bg-dark-mode-3  w-14 h-14 rounded-full flex justify-center">
-            <Image src="/user.svg" width={30} height={30} alt="soft skills" />
+            <Image
+              src="/softskills.svg"
+              width={35}
+              height={35}
+              alt="soft skills"
+              className="fill-current dark:invert"
+            />
           </div>
-          <p className="text-base font-normal transition ease-in-out delay-150 text-center group-hover:text-aqua2 w-24">
+          <p className="text-lg font-normal transition ease-in-out delay-150 text-center group-hover:text-aqua2 w-24">
             Soft Skills
+          </p>
+        </li>
+        <li className="group flex flex-col items-center gap-2 w-14">
+          <div className="bg-white dark:bg-dark-mode-3  w-14 h-14 rounded-full flex justify-center">
+            <Image
+              src="/experience.svg"
+              width={26}
+              height={26}
+              alt="soft skills"
+              className="fill-current dark:invert"
+            />
+          </div>
+          <p className="text-lg font-normal transition ease-in-out delay-150 text-center group-hover:text-aqua2 w-24">
+            Experiência
           </p>
         </li>
       </ul>
