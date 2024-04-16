0889975840141bc68d52a0fb795bc420b4ebc02f
Tabata Souto <tabatac.souto@gmail.com>
2024-04-16 18:13:54 -0300

    chore: correção mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,15 +11,18 @@ export default function Home() {
   const { menu, setMenu } = useContext(Context);
 
   return (
-    <div className="bg-[#EBEBEB] text-dark-mode-1 dark:bg-dark-mode-1 text dark:text-white text-lg tracking-wide flex flex-col h-full">
+    <div className="bg-[#EBEBEB] text-dark-mode-1 dark:bg-dark-mode-1 text dark:text-white text-lg tracking-wide flex flex-col justify-between h-screen">
       <DefaultHeader icon={setting} title="Configurações do console" />
-      <div className="">
+      <div className="grid-cols-[1fr,2fr] max-sm:flex max-sm:flex-col">
         <ul className="ml-6 relative flex flex-col gap-4 pt-4 bg-light-mode-2 dark:bg-dark-mode-2 max-sm:flex-row max-sm:pb-4 max-sm:ml-0 max-sm:gap-8">
-          <li className="cursor-pointer max-sm:pl-14" onClick={() => setMenu("temas")}>
+          <li
+            className="cursor-pointer max-sm:pl-14"
+            onClick={() => setMenu("temas")}
+          >
             {menu === "temas" && (
               <span className="absolute w-[3px] h-8 ml-12 bg-blue dark:bg-aqua max-sm:hidden"></span>
             )}
-           <p
+            <p
               className={`${
                 menu === "usuários" && "text-blue dark:text-aqua"
               } font-normal pl-14 dark:font-light dark:tracking-[0.05em] max-sm:px-2`}
